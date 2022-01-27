493f926fe65d2cc8aefee736340aa20aae9c0fec
Andrey <anruban2001@gmail.com>
2022-01-27 20:43:15 +0200

    Added again

--- ProductsCore/Models/Product.cs
+++ ProductsCore/Models/Product.cs
@@ -9,6 +9,7 @@ namespace ProductsCore.Models
         public double Price { get; set; }
         public Category CategoryProduct { get; set; }
         public bool IsAvailableToBuy { get; set; }
+        public int Number { get; set; }
         //TODO Add Sizes
     }
 }
